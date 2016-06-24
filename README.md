19256ca4fb7801d6417f6b8584cc9b8eca0829ba
jstrolen <2ferdan@gmail.com>
2016-06-24 02:55:18 +0200

    images

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Converts image into ASCII
 Very first version, no GUI nor CLI settings 
 
 Put image into C: and run
+
+![Cat](https://github.com/jstrolen/ASCII-Art/pics/cat.png)
+
+![Country](https://github.com/jstrolen/ASCII-Art/pics/country.png)
+
+![Desert](https://github.com/jstrolen/ASCII-Art/pics/desert.png)
--- src/GUI/Settings.java
+++ src/GUI/Settings.java
@@ -8,9 +8,9 @@ class Settings {
     static final boolean INIT_CREATE_IMAGE = true;
     static final boolean INIT_COLOR_IMAGE = true;
     static final boolean INIT_DETAIL = true;
-    static final int INIT_MAX_WIDTH = 200;
-    static final int INIT_MULTIPLIER = 10;
-    static final int INIT_FONT_SIZE = 12;
+    static final int INIT_MAX_WIDTH = 250;
+    static final int INIT_MULTIPLIER = 8;
+    static final int INIT_FONT_SIZE = 11;
 
     static final String DEFAULT_PATH = "C:\\";
     static final String[] DEFAULT_EXTENSIONS = new String[]{"gif", "png", "jpg", "bmp", "jpeg"};
